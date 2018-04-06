14aa489b4069401f9e6abbe69d5dbba2135ffb4b
nicholejeannine <nklerk@alopias.com>
2018-04-06 13:19:38 -0700

    For view components, we cannot set data equal to an object because the component is not tied to any single instance. However, we can make data return a function, which returns an object.

--- main.js
+++ main.js
@@ -1,49 +1,19 @@
 // it's best to use a hyphenated name so it doesn't interfere with any acutal Vue components.
 Vue.component('task-item', {
-	template: '<li>Foobar</li>'
+	template: '<li><slot></slot></li>'
+	// we cannot add a data object because this template is not tied to a specific instance.
+	// however we can set data equal to a function which RETURNS an object.
+	data(){
+		return {
+			message: "Foobar";
+		}
+	}
 });
 
 new Vue({
 	el: '#app'
+	// for a regular view instance, we can set data equal to an object:
+	data: {
+		// however you cannot do that for a component, because the component is not tied to a specific instance.
+	}
 });
-
-// var app = new Vue({
-// 	el: "#app",
-// 	data: {
-// 		tasks: [
-// 			{
-// 				description: "Go to the store",
-// 				completed: true
-// 			},
-// 			{
-// 				description: "Finish screencast",
-// 				completed: false
-// 			},
-// 			{
-// 				description: "Work on resume",
-// 				completed: false
-// 			},
-// 			{
-// 				description: "Do another thing",
-// 				completed: true
-// 			},
-// 			{
-// 				description: "Make more tasks",
-// 				completed: true
-// 			}
-// 		],
-// 		message: "Hello World!"
-// 	},
-// 	// the computed object lets you compute properties before they are displayed on the page.
-// 	computed: {
-// 		incompleteTasks(){
-// 			return this.tasks.filter(task => !task.completed);
-// 		}
-// 	},
-
-// 	// any custom methods we write should be nested within the "methods" object:
-// 	methods: {
-		
-// 	},
-
-// });
