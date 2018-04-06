f3f9e080b15dea531f4bbfc240b5ed1c65d85cbc
nicholejeannine <nklerk@alopias.com>
2018-04-06 15:06:02 -0700

    Use v-show to selectively show components based on a boolean value. Make a bootstrap 4 card that disappears on click

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 Vue.component('message', {
 	// you must register the properties that the template will accept.
 	props: ['title', 'body'],
-	template: `<div>
-	<div class="message">
-		<h2 class="text-primary text-center message-header">{{title}}
-		</h2>
-		<div class="message-body">
-			{{body}}
-		</div>
-	</div><hr/></div>`
+	// v-show: SHOW this component if the thing returns true. (There's also a v-hide directive)
+	template: `
+  		<div class="col-sm-6" v-show="isVisible">
+    		<div class="card">
+      			<div class="card-body">
+        			<h3 class="text-primary text-center card-title">{{title}}<button @click="hideCard" type="button" class="close" aria-label="Close">
+  <span aria-hidden="true">&times;</span>
+</button></h3>
+        			<p class="card-text">{{body}}</p>
+      			</div>
+    		</div>
+  		</div>`.trim(),
+
+  		// remember that data, when used inside a view component, must be a function which returns an object.
+  		data(){
+  			return {
+  				isVisible: true
+  			}
+  		},
+  		methods: {
+  			// this keyword refers to this specific instance, NOT everything on the page.
+  			hideCard() {
+  				// this is so basic that instead of this, you could use an onclick handler on the button: @click="isVisible = false"
+  				this.isVisible = false;
+  			}
+  		}
 });
 
 new Vue({
