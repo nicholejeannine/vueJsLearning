2cfef1507e1a1db18c52be971eda604dd58489b6
nicholejeannine <nklerk@alopias.com>
2018-04-10 17:22:10 -0700

    Dynamically change selected class on tabs - PROPERTIES ARE IMMUTABLE IN VUE - need to set a class by changing data

--- tabs.js
+++ tabs.js
@@ -1,11 +1,11 @@
 Vue.component('tabs', {
-
+	// in our template we will create a custom function to set the is-active tab on the class. We will call "selectTab" and pass the tab as an argument.
 	template: `
 	<div>
 		<div class="tabs">
  	 		<ul>
-    			<li v-for="tab in tabs">
-    				<a href="#">{{tab.name}}</a>
+    			<li v-for="tab in tabs" :class="{'is-active': tab.isActive}">
+    				<a href="#" @click="selectTab(tab)">{{tab.name}}</a>
     			</li>
   			</ul>
 		</div>
@@ -25,6 +25,24 @@ Vue.component('tabs', {
 		// we have to be explicit about the data that is returned, so declare that within the data function.
 		this.tabs = this.$children;
 	},
+	methods: {
+		selectTab(selectedTab){
+
+			/* // this function creates this error:
+			//[Vue warn]: Avoid mutating a prop directly since the value will be overwritten whenever the parent component re-renders. Instead, use a data or computed property based on the prop's value. Prop being mutated: "selected"
+			this.tabs.forEach(tab => {
+				tab.selected = (tab.name == selectedTab.name)
+			});
+			
+			IN GENERAL, consider the properties that we set in a component to be immutable.
+			If we do need mutability, use a computed property or "a piece of data"
+			*/
+			// What we do here to resolve the above problem: We set a piece of data on each tab (the property isActive, which we default to false. Then this method can go through and set the isActive piece of data, instead of setting properties).
+			this.tabs.forEach(tab => {
+				tab.isActive = (tab.name == selectedTab.name);
+			});
+		}
+	},
 	mounted(){
 		console.log(this.tabs);
 	}
@@ -34,10 +52,19 @@ Vue.component('tab', {
 
 	template: `<div><slot></slot></div>`,
 	// you must be explicit about the properties you pass in.
-	// you can require that properties are defined
 	// if you don't define the properties, they won't be available under the $vm0.$children.
+	// can require props, set defaults
 	props: {
-		name: {required: true}
+		name: {required: true},
+		selected: {default: false } // if you don't have a selected property set, make the default false.
+	},
+	data(){
+		return {
+			isActive: false
+		}
+	},
+	mounted() {
+		this.isActive = this.selected;
 	}
 });
 
