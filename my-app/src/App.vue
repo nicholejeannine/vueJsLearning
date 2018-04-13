7443d6ffa250a4bb0e91b4c5c895982b7684100d
nicholejeannine <nklerk@alopias.com>
2018-04-12 23:25:40 -0700

    Create a simple message component to import into the main App.vue file. So far we are just importing it, we are not DOING anything with it yet, so we don't see it display

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
+    <message>Hello There</message>
   </div>
 </template>
 
 <script>
+import Message from "./components/Message.vue";
 export default {
   name: 'app',
   data () {
---
+++ my-app/src/components/Message.vue
@@ -0,0 +1,30 @@
+<!-- A file ending in .vue can be a self-contained component. It has its template tag, its script tag, and its style tag, all in the same file. This can be good for very simple resuable components. -->
+
+<template>
+
+	<div class="box is-info">
+		<p>
+			<slot></slot>
+		</p>
+	</div>
+
+
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+// make it green so we know if it's there
+<style>
+
+.box {
+	background: green; 
+	padding: 10px;
+	border: 1px solid black;
+}
+
+</style>
