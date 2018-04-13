c1828f0d79de3bf1a862c48423954de709c0ecb9
nicholejeannine <nklerk@alopias.com>
2018-04-12 23:41:59 -0700

    Hot module replacement for front-end apps

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <div id="app">
-    <message>Hello There {{count}}</message>
-    <message>This is ANOTHER message!</message>
+    <!-- <message>Hello There {{count}}</message>
+    <message>This is ANOTHER message!</message> -->
+    <counter></counter>
   </div>
 </template>
 
 <script>
 // import the Message component - this is only importing it, we haven't DONE anything with it yet.
-import Message from "./components/Message.vue";
+// import Message from "./components/Message.vue";
+import Counter from "./components/Counter.vue";
 export default {
   name: 'app',
   // by including it in the "components" section, we are telling this app to load the named component. (if you comment this out, nothing happens)
-  components: {Message},
+  // components: {Message},
+  components: {Counter},
   data () {
-    return {
-      count: 1
-    }
+    
   }
 }
 </script>
---
+++ my-app/src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<!-- Hot module replacement means I can use live reload, and on the fly I can make changes to the template, the styling, etc, but this count variable retains its state. IT DOES NOT RESET EVERY TIME. Pretty cool.-->
+		<h2> The count: {{count}} </h2>
+
+		<p>
+			<button @click="count +=1">Increment Count</button>
+		</p>
+	</div>
+</template>
+
+<script>
+
+export default{
+
+	data(){
+		return {
+			count: 0
+		}
+	}
+}
+
+</script>
+
+<style>
+
+</style>
