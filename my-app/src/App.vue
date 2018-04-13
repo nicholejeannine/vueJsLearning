107a19f6a248e4982d81f16a9e7746def99e8f3b
nicholejeannine <nklerk@alopias.com>
2018-04-13 16:43:19 -0700

    Axios API for easy ajax requests in a laravel project

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -2,7 +2,13 @@
   <div id="app">
     <!-- <message>Hello There {{count}}</message>
     <message>This is ANOTHER message!</message> -->
-    <counter></counter>
+    <!-- <counter></counter> -->
+    <ul>
+      <!-- Issues might arise here if you are using a blade.php file - here, {{skill}} refers to a variable on our Vue instance; however, PHP will see this and attempt to render it as blade syntax. So we will get an undefined constant "skill" error. -->
+      <!-- TO HAVE THE BLADE COMPILER IGNORE THIS AND TREAT IT AS JS, add the @ symbol before the {{}} -->
+      <li for="skill in skills">@{{skill}}</li>
+      <!-- Another option would be to use v-text: -->
+      <li for="skill in skills" v-text="{{skill}}"></li>
   </div>
 </template>
 
--- my-app/src/main.js
+++ my-app/src/main.js
@@ -3,5 +3,16 @@ import App from './App.vue'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  render: h => h(App),
+  data: {
+  	skills: []
+  },
+  // here's how you might handle an ajax request with the Axios library:
+  mounted(){
+  	// maybe we are fetching a list of skills from the server. It would default to an empty array.
+  	axios.get('/skills').then(this.skills = response.data); // data will be returned in the response.data key
+  }
+});
+
+
+
