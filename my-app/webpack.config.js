0bbaf65be651a7aaf0383f49c4c13915322962ad
nicholejeannine <nklerk@alopias.com>
2018-04-12 22:46:42 -0700

    30-second tutorial on webpack config

--- my-app/webpack.config.js
+++ my-app/webpack.config.js
@@ -20,17 +20,22 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
+
+          // loaders give us a way to apply preprocessing to anything that we will require in. (import or require statement)
           loaders: {
           }
           // other vue-loader options go here
         }
       },
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
+        // use the babel-loader to compile babel into something all browsers can read. 
+        // apply this for anything with .js, UNLESS it's in node_modules dir
+        test: /\.js$/,   // which files do I apply this loader to?
+        loader: 'babel-loader',   // which loader do I use?
+        exclude: /node_modules/  // any files or patterns I should ignore?
       },
       {
+        // file-loader allows us to move image files anywhere we like
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
@@ -41,6 +46,7 @@ module.exports = {
   },
   resolve: {
     alias: {
+
       'vue$': 'vue/dist/vue.esm.js'
     },
     extensions: ['*', '.js', '.vue', '.json']
@@ -56,6 +62,8 @@ module.exports = {
   devtool: '#eval-source-map'
 }
 
+
+// check if we are in production. If we are, update source mapping, minimize js/css.
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
