92bf3f56c4c46f222efe14998ae2076b0315c8f7
nicholejeannine <nklerk@alopias.com>
2018-04-18 12:57:16 -0700

    Add Axios CDN javascript library

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ ini_set('include_path', get_include_path() . PATH_SEPARATOR . __DIR__);
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.css" crossorigin="anonymous">
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 
 </head>
