3a9b09e853acfd6da3fa8652472e33061f0cd9ac
nicholejeannine <nklerk@alopias.com>
2018-04-30 12:48:28 -0700

    ObjectOrientedForms Part1a: es6 Errors class to handle displaying errors on form

--- js/objectOrientedForms.js
+++ js/objectOrientedForms.js
@@ -1,18 +1,47 @@
+class Errors {
+
+	constructor(){
+		this.errors = {};
+	}
+
+	// get the errors for a given field
+	get(field){
+		if (this.errors[field]){
+			return this.errors[field][0];
+		}
+	}
+
+	record(errors){
+		this.errors = errors;
+	}	
+};
+
 new Vue({
 	el: '#app', 
 	data: {
 		name: '',
-		description: ''
+		description: '',
+		errors: new Errors()
 	},
 	methods: {
 		onSubmit(){
-
-			// alert("Form was submitted");
+			alert("Form was submitted");
 			// at this point we would want to use axios to submit a post request:
-			axios.post('/projects', {
-				name: this.name,
-				description: this.description
-			});
+			axios.post('/projects', this.$data)
+			// this.$data is the same as: {
+			// 	name: this.name,
+			// 	description: this.description
+			// });
+			.then(response => alert('success'))
+			.catch(error => {
+				// original way before adding the Errors class:
+				// if there is an error, add it to this.errors in the data object above.
+				// this.errors = error.response.data
+				// console.log(error.response.data);
+
+				// after adding the Errors class:
+				this.errors.record(error.response.data);
+			})
 		}
 	}
 });
--- main.js
+++ main.js
@@ -1,34 +1,3 @@
-Vue.component('message', {
-	// you must register the properties that the template will accept.
-	props: ['title', 'body'],
-	// v-show: SHOW this component if the thing returns true. (There's also a v-hide directive)
-	template: `
-  		<div class="col-sm-6" v-show="isVisible">
-    		<div class="card">
-      			<div class="card-body">
-        			<h3 class="text-primary text-center card-title">{{title}}<button @click="hideCard" type="button" class="close" aria-label="Close">
-  <span aria-hidden="true">&times;</span>
-</button></h3>
-        			<p class="card-text">{{body}}</p>
-      			</div>
-    		</div>
-  		</div>`.trim(),
-
-  		// remember that data, when used inside a view component, must be a function which returns an object.
-  		data(){
-  			return {
-  				isVisible: true
-  			}
-  		},
-  		methods: {
-  			// this keyword refers to this specific instance, NOT everything on the page.
-  			hideCard() {
-  				// this is so basic that instead of this, you could use an onclick handler on the button: @click="isVisible = false"
-  				this.isVisible = false;
-  			}
-  		}
-});
-
 new Vue({
 	el: '#app', 
 	// for a regular view instance, we can set data equal to an object:
