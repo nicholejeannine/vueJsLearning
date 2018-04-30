2a81b1d3301bf73c0522d84564a983296e536b6b
nicholejeannine <nklerk@alopias.com>
2018-04-30 13:38:24 -0700

    Object Oriented Forms Part 1b: on success, clear form inputs; add helper methods 'has' and 'any' to determine whether to display the error messages or disable the submit button

--- js/objectOrientedForms.js
+++ js/objectOrientedForms.js
@@ -18,6 +18,16 @@ class Errors {
 	clear(field) {
 		delete this.errors[field];
 	}	
+
+	has(field){
+		return this.errors.hasOwnProperty(field);
+	}
+
+	// check if there are ANY errors - if so we want to disable the submit button
+	any(){
+		// we're not using any helper libraries like lodash or underscore, so we can use the built-in javascript Object.keys, which returns an array, and check if the size > 0
+		return Object.keys(this.errors).length > 0;
+	}
 };
 
 new Vue({
@@ -29,14 +39,13 @@ new Vue({
 	},
 	methods: {
 		onSubmit(){
-			alert("Form was submitted");
 			// at this point we would want to use axios to submit a post request:
 			axios.post('/projects', this.$data)
 			// this.$data is the same as: {
 			// 	name: this.name,
 			// 	description: this.description
 			// });
-			.then(response => alert('success'))
+			.then(this.onSuccess) // why don't we have to pass the response?
 			.catch(error => {
 				// original way before adding the Errors class:
 				// if there is an error, add it to this.errors in the data object above.
@@ -46,6 +55,14 @@ new Vue({
 				// after adding the Errors class:
 				this.errors.record(error.response.data);
 			})
+		},
+		onSuccess(response) {
+			alert("We would return response.data.message if this was returned from the backend. This method will now clear out the input fields.");
+			// clear the form inputs
+			this.name = '';
+			this.description = '';
+
+			// in the next episode we will change this method to use a form object so we can do something like form.reset() instead of manually clearing out each field.
 		}
 	}
 });
